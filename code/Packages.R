f9e2d828bede86a4d44034845322a28164d727c5
shivam7898 <shivam7898@users.noreply.github.com>
2022-07-10 17:21:13 +0530

    add: csv

--- code/Packages.R
+++ code/Packages.R
@@ -1,7 +1,7 @@
 if (!("conflicted" %in% (.packages()))) {
   pkg <- character(0)
   pkg <- c(pkg, "conflicted", "knitr", "reticulate", "fontawesome")
-  pkg <- c(pkg, "lobstr", "tibble", "dplyr")
+  pkg <- c(pkg, "lobstr", "tibble", "dplyr", "here", "readr")
   
   lapply(pkg, FUN = \(x) library(x, character.only = TRUE))
   
