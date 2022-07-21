e19a28802bd4c56a86a50670dc910838ef26373c
shivam7898 <shivam7898@users.noreply.github.com>
2022-07-22 02:13:29 +0530

    add: Standard Datasets

--- code/Modules.py
+++ code/Modules.py
@@ -2,6 +2,10 @@ if (False or 'pandas' not in sys.modules):
     # CAUTION: Avoid implicit import i.e. 'from x import y'
     import sys, os, pip, copy, types, math
     import pandas as pd, numpy as np
+    import statsmodels.api as sm
+    import seaborn as sns
+    import sklearn
+    import sklearn.datasets
     # CAUTION: Due to R "arrow" & Python "pyarrow" conflict do not load it in R
     import pyarrow
     import pyarrow.feather
