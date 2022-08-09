0a7abbea1f3359c23bf18e0fdad070c336005afa
shivam7898 <shivam7898@users.noreply.github.com>
2022-08-09 19:30:29 +0530

    add: parallel execution using all processors

--- code/Modules.py
+++ code/Modules.py
@@ -1,6 +1,11 @@
 if (False or 'pandas' not in sys.modules):
     # CAUTION: Avoid implicit import i.e. 'from x import y'
-    import sys, os, pip, copy, types, math
+    import sys, os, multiprocessing
+    q_EXE_PATH = os.path.join(sys.exec_prefix, 'pythonw.exe')
+    sys.executable = q_EXE_PATH
+    sys._base_executable = q_EXE_PATH
+    multiprocessing.set_executable(q_EXE_PATH) 
+    import pip, copy, types, math, time
     import pandas as pd, numpy as np
     import statsmodels.api as sm
     import seaborn as sns
@@ -12,6 +17,9 @@ if (False or 'pandas' not in sys.modules):
     # CAUTION: Due to R "arrow" & Python "pyarrow" conflict do not load it in R
     import pyarrow
     import pyarrow.feather
+    import joblib
+    # No Scientific Notation in NumPy
+    np.set_printoptions(formatter={'float_kind':'{:f}'.format})
 else:
     print("Python Modules not loaded again.")
 
